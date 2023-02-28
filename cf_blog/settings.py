41e7fdc1d8aec4c9be57bb04bae3ca086634e140
joultrip <joulltrop@gmx.de>
2023-02-28 07:33:20 +0100

    sec

--- cf_blog/settings.py
+++ cf_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-go_s2-l!n8^s$j7avcrcrckt=+_)g45wfd7tk(m93h9&cqvs0s'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
